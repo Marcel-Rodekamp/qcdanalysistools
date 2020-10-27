ea1b683f2d4443cd0e0da88cb4a5cd8a0ad1e068
Marcel Rodekamp <mrodekamp@physik.uni-bielefeld.de>
2020-10-27 18:28:54 +0100

    Major bugfixes in estimator and variance, removed t_avg option for now

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -10,28 +10,29 @@ def _leave_out(t_data,t_num_ran_indices):
     from qcdanalysistools.jackknife import _leave_n_out_ran
     return _leave_n_out_ran(t_data,t_num_ran_indices,t_n=1)
 
-def bootstrap_var(t_data, t_num_ran_indices = None, t_subdata_sets = None, t_est = None, t_avg = np.average, **kwargs):
-    if t_est is None:
-        t_est = bootstrap_est(t_data, t_subdata_sets, t_num_ran_indices, t_avg, **kwargs)
+def bootstrap_var(t_data, t_num_ran_indices = None):
+    # create subdata sets
+    t_num_ran_indices = t_data.shape[0]//2 if t_num_ran_indices==None else t_num_ran_indices
+    subdata_sets = _leave_out(t_data,t_num_ran_indices=t_num_ran_indices)
 
-    if t_subdata_sets is None:
-        t_num_ran_indices = t_data.shape[0]//2 if t_num_ran_indices==None else t_num_ran_indices
-        t_subdata_sets = _leave_out(t_data,t_num_ran_indices=t_num_ran_indices)
+    # average in each block for index details see jackknife._leave_n_out_ran
+    # documentation.
+    est = np.average( subdata_sets, axis = 1 )
 
-    var = 0
-    for kth_est in t_subdata_sets:
-        var += np.square(kth_est - t_est)
+    return np.var( est, axis = 0 )
 
-    return var / t_data.shape[0]
+def bootstrap_est(t_data, t_num_ran_indices = None):
+    # create subdata sets
+    t_num_ran_indices = t_data.shape[0]//2 if t_num_ran_indices==None else t_num_ran_indices
+    subdata_sets = _leave_out(t_data,t_num_ran_indices=t_num_ran_indices)
 
-def bootstrap_est(t_data, t_subdata_sets = None, t_num_ran_indices=None, t_avg = np.average, **kwargs):
-    if t_subdata_sets is None:
-        t_num_ran_indices = t_data.shape[0]//2 if t_num_ran_indices==None else t_num_ran_indices
-        t_subdata_sets = _leave_out(t_data,t_num_ran_indices=t_num_ran_indices)
+    # determine and return the estimator
+    # the first (inner) average, averages in each block, the second (outer) does
+    # over the subdata sets, for index details see jackknife._leave_n_out_ran
+    # documentation.
+    return np.average(np.average(subdata_sets, axis = 1), axis = 0)
 
-    return t_avg(t_subdata_sets, axis = 0, **kwargs)
-
-def bootstrap(t_data, t_num_ran_indices = None, t_avg = np.average, t_blocked = False, t_num_blocks = None, **kwargs):
+def bootstrap(t_data, t_num_ran_indices = None, t_blocked = False, t_num_blocks = None):
     """
         t_data: numpy.ndarray
             Data array, containing the raw data of an observable or raw data on
@@ -65,10 +66,23 @@ def bootstrap(t_data, t_num_ran_indices = None, t_avg = np.average, t_blocked = 
     var = None
 
     if t_blocked:
-        pass
+        import qcdanalysistools.blocking as blocking
+        block_size = t_data.shape[0] // t_num_blocks
+        l_est = [None]*t_num_blocks
+        l_var = [None]*t_num_blocks
+
+        for block_id in range(0,t_num_blocks):
+            l_est[block_id] = bootstrap_est(blocking.get_block(t_data,block_id,block_size), t_num_ran_indices = t_num_ran_indices)
+            l_var[block_id] = bootstrap_var(blocking.get_block(t_data,block_id,block_size), t_num_ran_indices = t_num_ran_indices)
+
+        l_est[t_num_blocks-1] = bootstrap_est(blocking.get_block(t_data,t_num_blocks-1,block_size), t_num_ran_indices = t_num_ran_indices)
+        l_var[t_num_blocks-1] = bootstrap_var(blocking.get_block(t_data,t_num_blocks-1,block_size), t_num_ran_indices = t_num_ran_indices)
+
+        est = np.average(l_est,axis=0)
+        var = np.average(l_var,axis=0)
     else:
         # simple leave n out (randomnized)
-        est = bootstrap_est(t_data, t_avg = t_avg, t_num_ran_indices = t_num_ran_indices, **kwargs)
-        var = bootstrap_var(t_data, t_est = est, t_avg = t_avg, **kwargs)
+        est = bootstrap_est(t_data, t_num_ran_indices = t_num_ran_indices)
+        var = bootstrap_var(t_data, t_num_ran_indices = t_num_ran_indices)
 
     return est,var
