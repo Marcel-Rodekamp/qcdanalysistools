8d0eaf9218775c7832377ba7bd33f001d9e3e8d1
Marcel Rodekamp <mrodekamp@physik.uni-bielefeld.de>
2020-10-28 12:11:22 +0100

    Removed depricated argument

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -30,16 +30,13 @@ def bootstrap_var(t_data, t_num_ran_indices = None):
     # documentation.
     return np.var( np.average( subdata_sets, axis = 1 ), axis = 0 )
 
-def bootstrap(t_data, t_num_ran_indices = None, t_blocked = False, t_num_blocks = None):
+def bootstrap(t_data,t_blocked = False, t_num_blocks = None):
     """
         t_data: numpy.ndarray
             Data array, containing the raw data of an observable or raw data on
             which t_avg must be called. It is assumed, that axis=0 represents
             the data points of the method and subsequent axis' are assumed to
             represent multidimensional estimators.
-        t_avg: callable function, default: numpy.average
-            Variation of an average function or observable which is called to
-            determine the estimator on the (sub-)data sets.
         t_blocked: bool, default: False
             Set to `True` if the Bootstrap should be combined with the blocking
             method i.e.
@@ -50,11 +47,6 @@ def bootstrap(t_data, t_num_ran_indices = None, t_blocked = False, t_num_blocks 
         t_num_blocks: int, list of ints
             An integer which defines the number of blocks in which t_data is blocked
             once the blocking method is applied.
-        **kwargs:
-            keyworded arguments which can be passed to t_avg. Note that
-                axis
-            from numpy is going to be overwritten. This is due to the assumption
-            that axis=0 represents the data.
 
         Returns: numpy.ndarray, numpy.ndarray
             estimator, variance
@@ -70,11 +62,11 @@ def bootstrap(t_data, t_num_ran_indices = None, t_blocked = False, t_num_blocks 
         l_var = [None]*t_num_blocks
 
         for block_id in range(0,t_num_blocks):
-            l_est[block_id] = bootstrap_est(blocking.get_block(t_data,block_id,block_size), t_num_ran_indices = t_num_ran_indices)
-            l_var[block_id] = bootstrap_var(blocking.get_block(t_data,block_id,block_size), t_num_ran_indices = t_num_ran_indices)
+            l_est[block_id] = bootstrap_est(blocking.get_block(t_data,block_id,block_size))
+            l_var[block_id] = bootstrap_var(blocking.get_block(t_data,block_id,block_size))
 
-        l_est[t_num_blocks-1] = bootstrap_est(blocking.get_block(t_data,t_num_blocks-1,block_size), t_num_ran_indices = t_num_ran_indices)
-        l_var[t_num_blocks-1] = bootstrap_var(blocking.get_block(t_data,t_num_blocks-1,block_size), t_num_ran_indices = t_num_ran_indices)
+        l_est[t_num_blocks-1] = bootstrap_est(blocking.get_block(t_data,t_num_blocks-1,block_size))
+        l_var[t_num_blocks-1] = bootstrap_var(blocking.get_block(t_data,t_num_blocks-1,block_size))
 
         est = np.average(l_est,axis=0)
         var = np.average(l_var,axis=0)
