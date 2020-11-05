0b0767dcf939b820f855b2469a39d619295625fc
Marcel Rodekamp <mrodekamp@physik.uni-bielefeld.de>
2020-11-05 11:33:19 +0100

    Afore mentioned test function for jackknife

---
+++ test/t_jackknife.py
@@ -0,0 +1,93 @@
+import numpy as np
+
+import qcdanalysistools.analysis as ana
+
+do_print = True
+
+data_shape = (102,12)
+
+def test_estimator():
+    # define trivial observable
+    def obs(x,alpha=2):
+        return np.power(np.average(x,axis=0),alpha)
+
+    test_data = np.ones( shape=data_shape )
+    vali_data = 1 #np.ones(  )
+
+    res = ana.jackknife_est(
+        test_data,
+        t_n = 10,
+        t_obs = obs,
+        t_random_leaveout = True,
+        t_num_ran_indices = 20,
+        alpha=20
+    )
+
+    if do_print:
+        print("Test data:",test_data)
+        print("Result   :",res)
+        print("Valid res:",vali_data - res)
+
+    if np.all(vali_data == res):
+        print("Estimator Works")
+
+def test_variance():
+    # define trivial observable
+    def obs(x,alpha=2):
+        return np.power(np.average(x,axis=0),alpha)
+
+    test_data = np.ones( shape=data_shape )
+    vali_data = 0
+
+    res = ana.jackknife_var(
+        test_data,
+        t_n = 1,
+        t_obs = obs,
+        t_random_leaveout = False,
+        t_num_ran_indices = 20,
+        alpha=20
+    )
+
+    if do_print:
+        print("Test data:",test_data)
+        print("Result   :",res)
+        print("Valid res:",vali_data - res)
+
+    if np.all(vali_data == res):
+        print("Variance Works")
+
+def test_jackknife():
+    # define trivial observable
+    def obs(x,alpha=2):
+        return np.power(np.average(x,axis=0),alpha)
+
+    test_data = np.ones( shape=data_shape )
+    vali_var = 0
+    vali_est = 1
+
+    est,var = ana.jackknife(
+        test_data,
+        t_obs = obs,
+        t_n = 1,
+        t_random_leaveout = False,
+        t_num_ran_indices = 20,
+        t_blocked = False,
+        t_num_blocks = True,
+        alpha=20
+    )
+
+    if do_print:
+        print("Test data:",test_data)
+        print("Estimator:",est)
+        print("Variance :",var)
+        print("ValidateE:",vali_est-est)
+        print("ValidateV:",vali_var-var)
+
+    if np.all(vali_est == est) and np.all(vali_var == var):
+        print("Jackknife Works")
+
+test_estimator()
+print("")
+test_variance()
+print("")
+test_jackknife()
