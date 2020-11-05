0cdd239a3c71c1f068000944b11625cf7c8af6e4
Marcel Rodekamp <mrodekamp@physik.uni-bielefeld.de>
2020-11-05 12:07:09 +0100

    Added t_obs to jackknife methods, tested on data = [[1...],[1...],...]

--- src/StatisticalAnalysis/bootstrap.py
+++ src/StatisticalAnalysis/bootstrap.py
@@ -1,16 +1,19 @@
-"""
+r"""
+    This file contains the
+        * Bootstrap
+    method.
 """
 import numpy as np
-from numpy import average
+from .jackknife import _leave_n_out_ran
 
-def _leave_out(t_data,t_num_ran_indices):
+def _leave_out(t_data,t_num_subdata_sets):
     """
         t_data: numpy.ndarray
-            Data which becomes bootstraped. It is assumed that axis = 0
-            represents the different data points in the set and all other axis'
-            account for the dimensionality of the estimator.
-        t_num_leave_outs: int
-            Defines how many subdata sets are created by bootstrap.
+            Data array, containing the raw data. It is assumed,
+            that axis=0 represents the data points of the method and subsequent
+            axis' are assumed to represent multidimensional estimators.
+        t_num_subdata_sets: int
+            List of random indices which are left out.
 
         Returns:
             A set of subdata where each subdata is the original data set but without
@@ -19,95 +22,101 @@ def _leave_out(t_data,t_num_ran_indices):
     # The bootstrap methods leaves 1 randomly chosen data point out.
     # Following function can be recycled for this pupose. Therefore, the
     # default case t_n = 1 is used.
-    from qcdanalysistools.jackknife import _leave_n_out_ran
-    return _leave_n_out_ran(t_data,t_num_ran_indices,t_n=1)
+    return _leave_n_out_ran(t_data,t_num_subdata_sets,t_n=1)
 
-def bootstrap_est(t_data,t_num_leave_outs):
+def bootstrap_est(t_data,t_num_subdata_sets = None,t_obs=np.average,**obs_kwargs):
     """
         t_data: numpy.ndarray
-            Data which becomes bootstraped. It is assumed that axis = 0
-            represents the different data points in the set and all other axis'
-            account for the dimensionality of the estimator.
-        t_num_leave_outs: int
-            Defines how many subdata sets are created by bootstrap.
+            Data array, containing the raw data. It is assumed,
+            that axis=0 represents the data points of the method and subsequent
+            axis' are assumed to represent multidimensional estimators.
+        t_num_ran_indices: int, default: None
+            Number of random datasets created by leave one random out. Defaults
+            to t_data.shape[0] // 2 if None is given.
+        t_obs: function, default: numpy.average
+            Observable which should be computed over t_data.
+        **obs_kwargs: keyworded arguments
+            Keyworded arguments passed to t_obs
 
         Returns: numpy.ndarray
-            Variance after bootstrapping for each dimension of the estimator. Let
-            d be the total dimension of the estimator, n_i denotes the number
-            of elements in that particular dimension.
-                var.shape = (n_1,n_2,...,n_d) = t_data.shape[1:]
+            Estimator
 
-        This functions leaves a data point, which is chosen randomly, out (bootstrap)
-        Then, the estimator (numpy.average) on each of these subdata sets is evaluated.
-        The variance is determined by
-            var = 1/N sum_{k=1}^K (Theta_k - Theta)^2
-        where N is the data size, Theta_k is the estimator on the kth subdata set
-        and
-            Theta = 1/K sum_{k=1}^K Theta_k
+        This function uses the bootstrap method to determine the estimator of the
+        observable determined over t_data.
+        Let $N$ be the size of the data set. Let $X = {x_i}_{i\in[N]}$ denote
+        the total data set and K be the number of subdata_sets
+            1. On data X perform leave one random out --> ${X_k}_{k\in[0,K-1]}$
+            2. Compute observable on each subdata set --> $\Theta_k=\Theta(X_k)$
+            3. Compute estimator                      --> $\tilde{\Theta} = \frac{1}{K} \sum_{k\in[0,K-1]} \Theta_k$
+            4. Return $\tilde{\Theta}$
     """
-    # create subdata sets
-    t_num_ran_indices = t_data.shape[0]//2 if t_num_ran_indices==None else t_num_ran_indices
-    subdata_sets = _leave_out(t_data,t_num_ran_indices=t_num_leave_outs)
+    # 1. create subdata sets
+    t_num_subdata_sets = t_data.shape[0]//2 if t_num_subdata_sets==None else t_num_subdata_sets
+    subdata_sets = _leave_out(t_data,t_num_subdata_sets=t_num_subdata_sets)
 
-    # determine and return the estimator
-    # the first (inner) average, averages in each block, the second (outer) does
-    # over the subdata sets, for index details see jackknife._leave_n_out_ran
-    # documentation.
-    return np.average(np.average(subdata_sets, axis = 1), axis = 0)
+    # 2. Compute observables
+    Theta_k = np.zeros( shape = (t_num_subdata_sets,*t_data.shape[1:]) )
+    for k,x_k in enumerate(subdata_sets):
+        Theta_k[k] = t_obs(x_k,**obs_kwargs)
 
-def bootstrap_var(t_data,t_num_leave_outs):
-    """
+    # 3. determine estimator
+    return np.average(Theta_k, axis = 0)
+
+def bootstrap_var(t_data,t_num_subdata_sets = None,t_obs=np.average,**obs_kwargs):
+    r"""
         t_data: numpy.ndarray
-            Data which becomes bootstraped. It is assumed that axis = 0
-            represents the different data points in the set and all other axis'
-            account for the dimensionality of the estimator.
-        t_num_leave_outs: int
-            Defines how many subdata sets are created by bootstrap.
+            Data array, containing the raw data. It is assumed,
+            that axis=0 represents the data points of the method and subsequent
+            axis' are assumed to represent multidimensional estimators.
+        t_num_subdata_sets: int, default: None
+            Number of random datasets created by leave one random out. Defaults
+            to t_data.shape[0] // 2 if None is given.
+        t_obs: function, default: numpy.average
+            Observable which should be computed over t_data.
+        **obs_kwargs: keyworded arguments
+            Keyworded arguments passed to t_obs
 
         Returns: numpy.ndarray
-            Variance after bootstrapping for each dimension of the estimator. Let
-            d be the total dimension of the estimator, n_i denotes the number
-            of elements in that particular dimension.
-                var.shape = (n_1,n_2,...,n_d) = t_data.shape[1:]
+            Variance
 
-        This functions leaves a data point, which is chosen randomly, out (bootstrap)
-        Then, the estimator (numpy.average) on each of these subdata sets is evaluated.
-        The variance is determined by
-            var = 1/N sum_{k=1}^K (Theta_k - Theta)^2
-        where N is the data size, Theta_k is the estimator on the kth subdata set
-        and
-            Theta = 1/K sum_{k=1}^K Theta_k
+        This function uses the bootstrap method to determine the estimator of the
+        observable determined over t_data.
+        Let $N$ be the size of the data set. Let $X = {x_i}_{i\in[N]}$ denote
+        the total data set and K be the number of subdata sets
+            1. On data X perform leave one random out --> ${X_k}_{k\in[0,N-1]}$
+            2. Compute observable on each subdata set --> $\Theta_k=\Theta(X_k)$
+            3. Compute estimator                      --> $\tilde{Theta} = \frac{1}{K}\sum_{k\in[0,K-1]} \Theta_k$
+            4. Compute variance                       --> $\sigma^2 = \frac{1}{K} \sum_{k\in[0,K-1]} \left(\Theta_k-\tilde{\Theta}\right)^2$
     """
-    # create subdata sets
-    t_num_ran_indices = t_data.shape[0]//2 if t_num_ran_indices==None else t_num_ran_indices
-    subdata_sets = _leave_out(t_data,t_num_ran_indices=t_num_leave_outs)
+    # 1. create subdata sets
+    t_num_subdata_sets = t_data.shape[0]//2 if t_num_subdata_sets==None else t_num_subdata_sets
+    subdata_sets = _leave_out(t_data,t_num_subdata_sets=t_num_subdata_sets)
 
-    # average in each block for index details see jackknife._leave_n_out_ran
-    # documentation.
-    return np.var( np.average( subdata_sets, axis = 1 ), axis = 0 )
+    # 2. Compute observables
+    Theta_k = np.zeros( shape = (t_num_subdata_sets,*t_data.shape[1:]) )
+    for k,x_k in enumerate(subdata_sets):
+        Theta_k[k] = t_obs(x_k,**obs_kwargs)
 
-def bootstrap(t_data,t_num_leave_outs, t_blocked = False, t_num_blocks = None):
-    """
+    # 3,4. determine variance
+    return np.var(Theta_k, axis = 0)
+
+
+def bootstrap(t_data,t_num_subdata_sets = None, t_obs = np.average, t_blocked = False, t_num_blocks = None, **obs_kwargs):
+    r"""
         t_data: numpy.ndarray
-            Data array, containing the raw data of an observable or raw data on
-            which t_avg must be called. It is assumed, that axis=0 represents
-            the data points of the method and subsequent axis' are assumed to
-            represent multidimensional estimators.
-        t_num_leave_outs: int
-            Defines how many subdata sets are created by bootstrap.
-        t_blocked: bool, default: False
-            Set to `True` if the Bootstrap should be combined with the blocking
-            method i.e.
-                * t_data is blocked into t_num_blocks subdata blocks
-                * On each subblock a bootstrap is performed
-                * A simple average of the bootstrap estimators and variances is returned
-            In that case `t_num_blocks` must not be `None`!
-        t_num_blocks: int, list of ints
-            An integer which defines the number of blocks in which t_data is blocked
-            once the blocking method is applied.
+            Data array, containing the raw data. It is assumed,
+            that axis=0 represents the data points of the method and subsequent
+            axis' are assumed to represent multidimensional estimators.
+        t_num_subdata_sets: int, default: None
+            Number of random datasets created by leave one random out. Defaults
+            to t_data.shape[0] // 2 if None is given.
+        t_obs: function, default: numpy.average
+            Observable which should be computed over t_data.
+        **obs_kwargs: keyworded arguments
+            Keyworded arguments passed to t_obs
 
-        Returns: numpy.ndarray, numpy.ndarray
-            estimator, variance
+        Returns: numpy.ndarray
+            Estimator,Variance
     """
     # define return values
     est = None
@@ -120,17 +129,17 @@ def bootstrap(t_data,t_num_leave_outs, t_blocked = False, t_num_blocks = None):
         l_var = [None]*t_num_blocks
 
         for block_id in range(0,t_num_blocks):
-            l_est[block_id] = bootstrap_est(blocking.get_block(t_data,block_id,block_size),t_num_leave_outs=t_num_leave_outs)
-            l_var[block_id] = bootstrap_var(blocking.get_block(t_data,block_id,block_size),t_num_leave_outs=t_num_leave_outs)
+            l_est[block_id] = bootstrap_est(blocking.get_block(t_data,block_id,block_size),t_num_leave_outs=t_num_leave_outs,t_obs=t_obs,**obs_kwargs)
+            l_var[block_id] = bootstrap_var(blocking.get_block(t_data,block_id,block_size),t_num_leave_outs=t_num_leave_outs,t_obs=t_obs,**obs_kwargs)
 
-        l_est[t_num_blocks-1] = bootstrap_est(blocking.get_block(t_data,t_num_blocks-1,block_size),t_num_leave_outs=t_num_leave_outs)
-        l_var[t_num_blocks-1] = bootstrap_var(blocking.get_block(t_data,t_num_blocks-1,block_size),t_num_leave_outs=t_num_leave_outs)
+        l_est[t_num_blocks-1] = bootstrap_est(blocking.get_block(t_data,t_num_blocks-1,block_size),t_num_leave_outs=t_num_leave_outs,t_obs=t_obs,**obs_kwargs)
+        l_var[t_num_blocks-1] = bootstrap_var(blocking.get_block(t_data,t_num_blocks-1,block_size),t_num_leave_outs=t_num_leave_outs,t_obs=t_obs,**obs_kwargs)
 
         est = np.average(l_est,axis=0)
         var = np.average(l_var,axis=0)
     else:
         # simple leave n out (randomnized)
-        est = bootstrap_est(t_data,t_num_leave_outs=t_num_leave_outs)
-        var = bootstrap_var(t_data,t_num_leave_outs=t_num_leave_outs)
+        est = bootstrap_est(t_data,t_num_leave_outs=t_num_leave_outs,t_obs=t_obs,**obs_kwargs)
+        var = bootstrap_var(t_data,t_num_leave_outs=t_num_leave_outs,t_obs=t_obs,**obs_kwargs)
 
     return est,var
