270600a3a97e3f1e75185d102d39d337f0c58f00
Marcel Rodekamp <mrodekamp@physik.uni-bielefeld.de>
2020-11-06 10:22:50 +0100

    Changed to simple cosh, introduced fit range

--- src/Observables/first_order_energy.py
+++ src/Observables/first_order_energy.py
@@ -9,18 +9,25 @@ r"""
     This file contains methods to extract 1st order energy levels from two point
     correlation functions C(t). Thus it is assumed that for $t\to\infty$
         $$
-            C(t) \sim A_0 e^{-t E_0} + A_0 e^{-(N_t - t) E_0}
-                  = 2 A_0 e^{- \frac{N_t E_0}{2}} \cosh\left(\left( \frac{N_t}{2} - t \right) E_0\right)
+            C(t) = A_0 \cosh\left(\left( t - \frac{N_t}{2} \right) E_0\right)
         $$
     A periodic temporal direction is assumed
 """
 
-def energy_firstorder(t_correlator, t_A0 = 1, t_E0=1,t_analysis_type="Plain", t_plot_path=None, **analysis_kwargs):
+def energy_firstorder(t_correlator, t_fit_range = None, t_A0 = 1, t_E0=1, t_analysis_type="Plain", t_plot_path=None, **analysis_kwargs):
     r"""
         t_correlator: numpy.ndarray
             Data array, containing the raw data of two point correlation function.
             It is assumed, that axis=0 represents the data points of the method
             and axis=1 represents the temportal extend $t\in[0,N_t-1]$.
+        t_fit_range: tuple, default: None
+            The fit is performed in the range [ t_fit_range[0], t_fit_range[1] ). (upper bound is excluded)
+            If default (None) is given the fitrange is extended to the half axis
+            $t\in[0,(N_t-1)//2]$.
+        t_A0: float
+            Initial guess of $A_0$ for the fit.
+        t_E0: float
+            Initial guess of $E_0$ for the fit.
         t_analysis_type: string, default: "Plain"
             Sates which analysis method is chosen to estimate the correlator data
             Possibilities are:
@@ -47,7 +54,7 @@ def energy_firstorder(t_correlator, t_A0 = 1, t_E0=1,t_analysis_type="Plain", t_
         first energy level $E_0$ and the proportionality $A_0$ of the correlator
         behaviour
         $$
-            C(t) \sim A_0 e^{-t E_0}
+            C(t) = A_0 \cosh\left(\left( t - \frac{N_t}{2} \right) E_0\right)
         $$
         for large times $t\in\$. This is achived by a fit. The resulting fit and
         data is plotted and eventually stored in `plot_path`
@@ -78,21 +85,26 @@ def energy_firstorder(t_correlator, t_A0 = 1, t_E0=1,t_analysis_type="Plain", t_
     Nt = 2*corr_est.size
     # define temporal axis
     t_axis = np.array([t for t in range(corr_est.size)])
+    # set fit range
+    if t_fit_range is None:
+        t_fit_range = t_axis
+    else:
+        t_fit_range = np.array( [t for t in range(t_fit_range[0],t_fit_range[1])] )
     # define fit model
     correlator_fct = lambda t,A_0,E_0 : \
-        2*A_0*np.exp(-Nt*E_0/2)*np.cosh((Nt/2-t)*E_0)
+        A_0*np.cosh((t -Nt/2)*E_0)
     # perform fit, Results: popt = [A_0,E_0], pcov = [[ Var(A_0,A_0),Var(A_0,E_0) ],
     #                                                 [ Var(E_0,A_0),Var(E_0,E_0) ]]
     popt,pcov = scipy.optimize.curve_fit(correlator_fct,
-                                xdata = t_axis,
-                                ydata = corr_est,
+                                xdata = t_fit_range,
+                                ydata = corr_est[t_fit_range[0]:t_fit_range[-1]+1],
                                 p0    = [t_A0,t_E0],
-                                sigma = corr_err )
+                                sigma = corr_err[t_fit_range[0]:t_fit_range[-1]+1] )
 
-    corr_fit = correlator_fct(t_axis, popt[0], popt[1])
+    corr_fit = correlator_fct(t_fit_range, popt[0], popt[1])
 
     chisq, p_val = scipy.stats.chisquare(corr_fit,
-                                        f_exp = corr_est)
+                                        f_exp = corr_est[t_fit_range[0]:t_fit_range[-1]+1])
 
     report_string = "First level energy report:\n"
     report_string+= f"A_0 = {popt[0]} \u00B1 {np.sqrt(pcov[0][0])} \n"
@@ -110,9 +122,9 @@ def energy_firstorder(t_correlator, t_A0 = 1, t_E0=1,t_analysis_type="Plain", t_
                      fmt = '+',
                      yerr = corr_err,
                      label = r"$C(t)$")
-        plt.plot(t_axis,
+        plt.plot(t_fit_range,
                  corr_fit,
-                 label = r"$ C(t) = 2 A_0 e^{{- \frac{{N_t E_0 }}{{2}} }} \cosh\left(\left( \frac{{N_t}}{{2}} - t \right) E_0\right)$, $_{{A_0 = {0:.2g},\, E_0 = {1:.2g} }}$".format(*popt))
+                 label = r"$ C(t) = A_0 \cosh\left(\left( t - \frac{{N_t}}{{2}} \right) E_0\right)$, $_{{A_0 = {0:.2g},\, E_0 = {1:.2g} }}$".format(*popt))
 
         plt.grid()
         plt.xlabel("t")
