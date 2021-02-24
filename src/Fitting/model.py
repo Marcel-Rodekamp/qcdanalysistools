ccc8fda109e5e4bfe9e8c4d771ec43bd3e306b1d
Marcel Rodekamp <marcel.rodekamp@gmail.com>
2021-02-24 08:33:13 +0100

    Added EnergyGap,TwoExponential models

--- src/Fitting/model.py
+++ src/Fitting/model.py
@@ -134,7 +134,6 @@ class ExponentialModel(ModelBase):
 
         return np.array([dA,dB])
 
-
     def hess_param(self,t_x,t_A,t_B):
         r"""
             t_A: float
@@ -151,6 +150,121 @@ class ExponentialModel(ModelBase):
 
     def __name__(self):
         return f"f(x,A,B) = A*exp( -x*B )"
+
+class EnergyGapModel(ModelBase):
+    def __init__(self,t_A0,t_E0,t_A1,t_DE):
+        r"""
+            Equivalent to 2 exponential sum model but keeping $E_0$,$A_0$ fixed
+            $$
+                f(t) = e^{-t E_0} (A_0 + A_1 e^{-t(\Delta E)})
+            $$
+        """
+
+        super().__init__(t_num_params = 2, t_params0 = (t_A0,t_DE), t_param_names = ("A_1","DeltaE"))
+        self.A0 = t_A0
+        self.E0 = t_E0
+
+    def apply(self,t_x,t_A1,t_DE):
+        r"""
+            t_A: float
+                Scaling parameter
+            t_B: float
+                Exponent parameter
+        """
+
+        return np.exp(- t_x * self.E0)*(self.A0+t_A1*np.exp(- t_x * t_DE))
+
+    def grad_param(self,t_x,t_A1,t_DE):
+        dA = np.exp(-t_x *(t_DE+self.E0))
+
+        dB = -t_x*t_A1*np.exp(-t_x*(t_DE+self.E0))
+
+        return np.array([dA,dB])
+
+    def hess_param(self,t_x,t_A1,t_DE):
+        dA2 = np.zeros_like(t_x)
+        dB2 = t_x**2 * t_A1 * np.exp(-t_x*(t_DE+self.E0))
+        dAdB = -t_x * np.exp(-t_x*(t_DE+self.E0)) # = dBdA
+
+        return np.array( [ [dA2,dAdB],[dAdB,dB2] ] )
+
+    def __name__(self):
+        return f"f(x,A_1,E_1) = exp(-x*E_0) * (A_0 + A_1*exp(-x*(E_1-E_0))) | A_0,E_0 fixed"
+
+class TwoExponentialModel(ModelBase):
+    def __init__(self,t_A0,t_B0,t_C0,t_D0):
+        r"""
+            t_A0: float
+                Initial guess of the scaling parameter
+            t_B0: float
+                Initial guess of exponent parameter
+            t_C0: float
+                Initial guess of the scaling parameter
+            t_D0: float
+                Initial guess of exponent parameter
+
+            Exponential model:
+                $$
+                f(t,A,E) = A \cdot e^{- x \cdot B} + C \cdot e^{- x \cdot D}
+                $$
+        """
+        super().__init__(t_num_params = 4, t_params0 = (t_A0,t_B0,t_C0,t_D0), t_param_names = ("A","B","C","D"))
+
+    def apply(self,t_x,t_A,t_B,t_C,t_D):
+        r"""
+            t_A: float
+                Scaling parameter
+            t_B: float
+                Exponent parameter
+        """
+
+        return t_A * np.exp(- t_x * t_B) + t_C * np.exp(- t_x * t_D)
+
+    def grad_param(self,t_x,t_A,t_B,t_C,t_D):
+        r"""
+            t_A: float
+                Scaling parameter
+            t_B: float
+                Exponent parameter
+        """
+
+        dA = np.exp(-t_x * t_B)
+
+        dB = -t_x*t_A*np.exp(-t_x*t_B)
+
+        dC = np.exp(-t_x * t_D)
+
+        dD = -t_x*t_C*np.exp(-t_x*t_D)
+
+        return np.array([dA,dB,dC,dD])
+
+    def hess_param(self,t_x,t_A,t_B,t_C,t_D):
+        r"""
+            t_A: float
+                Scaling parameter
+            t_B: float
+                Exponent parameter
+        """
+
+        dA2 = np.zeros_like(t_x)
+        dB2 = t_x**2 * t_A * np.exp(-t_x*t_B)
+        dAdB = -t_x * np.exp(-t_x*t_B)                  # = dBdA
+
+        dC2 = np.zeros_like(t_x)
+        dD2 = t_x**2 * t_C * np.exp(-t_x*t_D)
+        dCdD =  -t_x * np.exp(-t_x*t_D)                 # = dDdC
+
+        dAdC = np.exp(-t_x * t_B) + np.exp(-t_x * t_D)  # = dCdA
+        dAdD = np.exp(-t_x * t_B) - t_x*t_C*np.exp(-t_x*t_D) # = dDdA
+
+        dBdC = -t_x*t_A*np.exp(-t_x*t_B) + np.exp(-t_x * t_D)
+        dBdD = -t_x*t_A*np.exp(-t_x*t_B)-t_x*t_C*np.exp(-t_x*t_D)
+
+
+        return np.array( [ [dA2,dAdB,dAdC,dAdD],[dAdB,dB2,dBdC,dBdD],[dAdC,dBdC,dC2,dCdD],[dAdD,dBdD,dCdD,dD2] ] )
+
+    def __name__(self):
+        return f"f(x,A,B) = A*exp( -x*B )+C*exp( -x*D )"
 
 class PropInversModel(ModelBase):
     def __init__(self,t_A0):
@@ -178,7 +292,7 @@ class PropInversModel(ModelBase):
             t_A: float
                 Scaling parameter
         """
-        
+
         return np.array([1/t_x])
 
     def hess_param(self,t_x,t_A):
