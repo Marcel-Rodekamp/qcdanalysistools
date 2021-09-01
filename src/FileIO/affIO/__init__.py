248eb0004d9908e3dc518e4f18132afc83cf2433
Marcel Rodekamp <marcel.rodekamp@gmail.com>
2021-09-01 09:41:00 +0200

    Depricated affIO src code still available but import is commented out

--- src/FileIO/affIO/__init__.py
+++ src/FileIO/affIO/__init__.py
@@ -1,5 +1,5 @@
-try:
-    from .read_aff import *
-except ModuleNotFoundError as e:
-    print(f"Submodule qcdanalysys.fileIO.affIO not available due to exception:\n{e}")
-    exit()
+#try:
+#    from .read_aff import *
+#except ModuleNotFoundError as e:
+#    print(f"Submodule qcdanalysys.fileIO.affIO not available due to exception:\n{e}")
+#    exit()
